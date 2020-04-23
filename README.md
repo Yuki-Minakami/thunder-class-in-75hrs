505969fb65be4d8480eff9b205505e3eebce731b
likaiboy <1140398276@qq.com>
2020-04-23 21:07:20 +0800

    day2 learn ffmpeg

--- README.md
+++ README.md
@@ -5,3 +5,10 @@
 实现基础的聊天室功能
 使用electron API获取实时屏幕
 
+
+##day2 2 小时
+花了半天了解了ffmpeg的工作方式，
+1. 需要在本地运行rtmp服务器-- 借助nginx
+2. 使用ffmpeg录屏并推送
+3. 在浏览器中解析并展示
+ffmpeg -f gdigrab -framerate 30 -offset_x 10 -offset_y 20 -video_size 640x480 -show_region 1 -i desktop -r 16 -vcodec h264 -acodec aac -f flv rtmp://localhost/myapp/stream
--- wsServer.js
+++ wsServer.js
@@ -2,6 +2,9 @@ const WebSocket = require('ws');
 //新建WebSocket服务
 const wss = new WebSocket.Server({ port: 3000 });
 
+const ffmpeg = require("fluent-ffmpeg");
+const websocketStream = require('websocket-stream/stream');
+
 var history = [];
 wss.on('connection', (ws)=> {
 
@@ -22,3 +25,28 @@ wss.on('connection', (ws)=> {
     //连接成功
     console.log("Connected");
 });
+
+function pushStream(ws){
+    const stream = WebSocket.createWebSocketStream(ws, { encoding: 'utf8' });
+ 
+
+    try {
+        ffmpeg("/live")
+            .addInputOption("-rtsp_transport", "tcp", "-buffer_size", "102400")  // 这里可以添加一些 RTSP 优化的参数
+            .on("start", function () {
+                console.log(url, "Stream started.");
+            })
+            .on("error", function (err) {
+                console.log(url, "An error occured: ", err.message);
+            })
+            .on("end", function () {
+                console.log(url, "Stream end!");
+                      // 摄像机断线的处理
+            })
+
+    } catch (error) {
+        console.log(error);
+    }
+    
+}
+
